93993db2f93abd1bb11a78857d5803a6f867ca41
Yamayamaaya <yugo139013@gmail.com>
2022-11-07 03:12:25 +0900

    アイテムボックス実装

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public GameObject Player;
+    Vector3 playerPos;
+    float distance;
+    Button btn;
+    Transform myTransform;
+
+
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+        myTransform = this.transform;
+    }
+    private void Update()
+    {
+        playerPos = Player.gameObject.transform.position;
+        distance = Vector3.Distance(myTransform.position, playerPos);
+        /*Debug.Log(distance);*/
+        btn.interactable = true;
+
+
+        if (distance < 0.5f)
+        {
+            btn.interactable = true;
+        }
+    }
+    // 複数アイテムの実装
+    // 必要なアイテムを列挙する
+    public enum ItemType
+    {
+        knife,
+        kaityuudento,
+        Key3,
+    }
+    // このアイテムが何なのか
+    public ItemType item;
+    // クリックされたら、非表示になってアイテムBoxに追加される
+    // タイミング：クリックされたら
+    // 処理：
+    // ・非表示
+    // ・アイテムBoxへ追加
+
+
+    // タイミング：クリックされたら
+    public void OnThis()
+    {
+        
+            gameObject.SetActive(false);
+            // アイテムBoxへ追加
+            int index = (int)item;
+            Debug.Log(index);
+            //ItemBox.instance.ReloadBox();
+            ItemBox.instance.SetItem(index);
+            // ・非表示
+       
+    }
+}
